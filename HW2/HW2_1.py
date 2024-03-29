1067d41cb49302db4597d3c98e71df1faf90fe83
Balan666 <113861516+Balan666@users.noreply.github.com>
2024-03-29 21:40:38 +0300

    Update HW2_1.py
    Changed ensembl regular string

--- HW2/HW2_1.py
+++ HW2/HW2_1.py
@@ -48,7 +48,7 @@ def parse_response_ensembl(decoded):
     return output
 
 def parse_ID(ID_list):
-    ensembl_match = all(re.search(r'(ENS(|MUS|RNO)G\d{11})|(Y[A-P][LR]\d{3}[CW](-[A-G])?)', 'r' + ID) for ID in ID_list)
+    ensembl_match = all(re.search(r'"(ENS(|\w*)|(MGP\D*))(E|FM|G|GT|P|RF|T)\d{11}"g', 'r' + ID) for ID in ID_list)
     uniprot_match = all(re.search(r'[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}', 'r' + ID) for ID in ID_list)
 
     if ensembl_match:
